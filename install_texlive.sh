f65d689bbbe0f85cd581541ccc2d3c36d79c6058
Silke Hofstra <silke@slxh.eu>
2018-07-19 17:43:21 +0200

    Initial commit

---
+++ install_texlive.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+REPO="http://mirror.ctan.org/systems/texlive/tlnet/"
+
+# Create temporary installation directory
+mkdir -p /tmp/texlive
+cd /tmp/texlive
+
+# Create installer configuration
+cat <<EOF >texlive.profile
+selected_scheme scheme-basic
+instopt_adjustpath 1
+tlpdbopt_install_docfiles 0
+tlpdbopt_install_srcfiles 0
+EOF
+
+# Retrieve installer
+wget -q "${REPO}/install-tl-unx.tar.gz"
+
+# Unpack
+tar xf install-tl-unx.tar.gz --strip-components=1
+
+# Install
+./install-tl -profile=texlive.profile
+
+cd /tmp/
+rm -rf /tmp/texlive
