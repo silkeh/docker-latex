45cd75c896a70da220e92834584ddde6223884ea
Silke Hofstra <silke@slxh.eu>
2018-07-19 21:34:55 +0200

    Add tetex

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 REPO="silkeh/latex"
-TAGS="basic small medium full debian debian-latest ubuntu"
+TAGS="basic small medium tetex full debian debian-latest ubuntu"
 
 for tag in ${TAGS}; do
     docker build . -f "Dockerfile.${tag}" -t "${REPO}:${tag}"
