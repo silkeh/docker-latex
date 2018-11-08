60a1aa6febe1cc03e73d0dcc44c36d08f37291fb
Silke Hofstra <silke@slxh.eu>
2018-11-08 12:14:56 +0100

    Add pandoc image

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 REPO="silkeh/latex"
-TAGS="basic small context medium tetex full debian debian-latest ubuntu"
+TAGS="basic small context medium tetex full pandoc debian debian-latest ubuntu"
 
 for tag in ${TAGS}; do
     docker build . -f "Dockerfile.${tag}" -t "${REPO}:${tag}"
