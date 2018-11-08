e7d1427d35511d27dcad248fe7769e0da63efd84
Silke Hofstra <silke@slxh.eu>
2018-11-08 13:34:41 +0100

    Improve build process

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 REPO="silkeh/latex"
-TAGS="basic small context medium tetex full pandoc debian debian-latest ubuntu"
+TAGS="basic small context medium tetex full pandoc debian debian-testing ubuntu"
 
 for tag in ${TAGS}; do
     docker build . -f "Dockerfile.${tag}" -t "${REPO}:${tag}"
